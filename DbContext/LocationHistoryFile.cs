33a7d76ce32a460036aa35c7120954096a5330a2
Alexandr Makhin <amakhin@gmail.com>
2020-07-13 20:40:53 +0200

    move folder

---
+++ DbContext/LocationHistoryFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BorderCrossing.DbContext
+{
+    public class LocationHistoryFile
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public DateTime DateUpload { get; set; }
+        
+        public string FileName { get; set; }
+        
+        public byte[] File { get; set; }
+    }
+}
---
+++ Models/LocationHistoryFilePostRequest.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace BorderCrossing.Models
+{
+    public class LocationHistoryFilePostRequest
+    {
+        public IFormFile LocationHistoryFile { set; get; }
+    }
+}
