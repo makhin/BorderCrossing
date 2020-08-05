b88fb72c80da66e8dc94b82e8c66180ffd98e568
Alexandr Makhin <amakhin@gmail.com>
2020-08-05 09:04:10 +0200

    add branch for UWP app

---
+++ BorderCrossing.UWP2/MainPage.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BorderCrossing.UWP2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public readonly string HomeLabel = "Главная";
+        public readonly string HowLabel = "Как?";
+        public readonly string UploadLabel = "Загрузить";
+
+        /// <summary>
+        /// Gets the navigation frame instance.
+        /// </summary>
+        public Frame AppFrame => frame;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Loaded += (sender, args) =>
+            {
+                NavView.SelectedItem = HomeMenuItem;
+            };
+        }
+
+        /// <summary>
+        /// Navigates to the page corresponding to the tapped item.
+        /// </summary>
+        private void NavigationView_ItemInvoked(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewItemInvokedEventArgs args)
+        {
+            var label = args.InvokedItem as string;
+            var pageType =
+                label == HomeLabel ? typeof(HomePage) :
+                label == UploadLabel ? typeof(UploadPage) :
+                label == HowLabel ? typeof(HowPage) : null;
+            if (pageType != null && pageType != AppFrame.CurrentSourcePageType)
+            {
+                AppFrame.Navigate(pageType);
+            }
+        }
+
+        /// <summary>
+        /// Ensures the nav menu reflects reality when navigation is triggered outside of
+        /// the nav menu buttons.
+        /// </summary>
+        private void OnNavigatingToPage(object sender, NavigatingCancelEventArgs e)
+        {
+            if (e.NavigationMode == NavigationMode.Back)
+            {
+                if (e.SourcePageType == typeof(HomePage))
+                {
+                    NavView.SelectedItem = HomeMenuItem;
+                }
+                else if (e.SourcePageType == typeof(HowPage))
+                {
+                    NavView.SelectedItem = HowMenuItem;
+                }
+                else if (e.SourcePageType == typeof(UploadPage))
+                {
+                    NavView.SelectedItem = UploadMenuItem;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Navigates the frame to the previous page.
+        /// </summary>
+        private void NavigationView_BackRequested(Microsoft.UI.Xaml.Controls.NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewBackRequestedEventArgs args)
+        {
+            if (AppFrame.CanGoBack)
+            {
+                AppFrame.GoBack();
+            }
+        }
+    }
+}
---
+++ BorderCrossing.UWP2/UploadPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace BorderCrossing.UWP2
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class UploadPage : Page
+    {
+        public UploadPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.List;
+            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Downloads;
+            picker.FileTypeFilter.Add(".zip");
+
+            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+            if (file != null)
+            {
+            }
+            else
+            {
+            }
+        }
+    }
+}
